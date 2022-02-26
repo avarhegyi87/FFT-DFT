61f0665bd6874ff6f5faa8aa19970acc0b2e47ce
avarhegyi87 <42640245+avarhegyi87@users.noreply.github.com>
2022-02-26 15:14:25 +0100

    Add project files.

---
+++ FFT-DFT/Complex.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFT_DFT {
+    enum ComplexType {
+        Real, Imaginary, UpperRight, BottomRight, UpperLeft, BottomLeft
+    }
+    class Complex : IComparable<Complex>, ICloneable {
+        private double x;
+        private double y;
+        private double absVal;
+        private ComplexType numType;
+
+        public Complex(double a, double b, bool cartesian = true) {
+            if (cartesian) {
+                x = a;
+                y = b;
+                GetAbsVal();
+            }
+            else {
+                //double rad = b % (2 * Math.PI);
+                x = Math.Round(a * Math.Cos(b), 6);
+                y = Math.Round(a * Math.Sin(b), 6);
+                absVal = a;
+            }
+            GetLocation();
+        }
+        
+        public double X {
+            get { return x; }
+            set { 
+                x = value;
+                GetAbsVal();
+                GetLocation();
+            }
+        }
+        public double Y {
+            get { return y; }
+            set { 
+                y = value;
+                GetAbsVal();
+                GetLocation();
+            }
+        }
+        public double AbsVal {
+            get { return Math.Round(absVal,6); }
+        }
+        public double this[int index] { get {
+                switch (index) {
+                    case 0: return x;
+                    case 1: return y;
+                    case 2: return absVal;
+                    default:
+                        throw new IndexOutOfRangeException("Index out of range...");
+                }
+            } 
+        }
+
+        public string Location {
+            get {
+                switch (this.numType) {
+                    case ComplexType.Real:
+                        return "Real";
+                    case ComplexType.Imaginary:
+                        return "Imaginary";
+                    case ComplexType.UpperRight:
+                        return "Upper right";
+                    case ComplexType.BottomRight:
+                        return "Bottom right";
+                    case ComplexType.UpperLeft:
+                        return "Upper left";
+                    case ComplexType.BottomLeft:
+                        return "Bottom left";
+                    default:
+                        return "undefined";
+                }
+            }
+        }
+
+        private void GetLocation() {
+            if (y == 0) numType = ComplexType.Real;
+            else if (x == 0) numType = ComplexType.Imaginary;
+            else if (x > 0 && y > 0) numType = ComplexType.UpperRight;
+            else if (x < 0 && y > 0) numType = ComplexType.UpperLeft;
+            else if (x < 0 && y < 0) numType = ComplexType.BottomLeft;
+            else numType = ComplexType.BottomRight;
+        }
+
+        private void GetAbsVal() {
+            absVal = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public int CompareTo(Complex other) {
+            return absVal.CompareTo(other.absVal);
+        }
+
+        public object Clone() {
+            return new Complex(x, y);
+        }
+
+        public static Complex operator+(Complex z1, Complex z2) => new Complex(z1.X + z2.X, z1.Y + z2.Y);
+
+        public static Complex operator-(Complex z1, Complex z2) => new Complex(z1.X - z2.X, z1.Y - z2.Y);
+
+        public static Complex operator*(Complex z1, Complex z2) {
+            double z = z1.absVal * z2.absVal;
+            double phi = Math.Atan2(z1.Y, z1.X) + Math.Atan2(z2.Y, z2.X);
+            return new Complex(Math.Round(z * Math.Cos(phi), 6), Math.Round(z * Math.Sin(phi), 6));
+        }
+
+        public static Complex operator/(Complex z1, Complex z2) {
+            double z = z1.absVal / z2.absVal;
+            double phi = Math.Atan2(z1.Y, z1.X) - Math.Atan2(z2.Y, z2.X);
+            return new Complex(Math.Round(z * Math.Cos(phi), 6), Math.Round(z * Math.Sin(phi), 6));
+        }
+
+        public override string ToString() {
+            switch (this.numType) {
+                case ComplexType.Real:
+                    return String.Format("{0}", Math.Round(X, 6));
+                case ComplexType.Imaginary:
+                    return String.Format("{0}j", Math.Round(Y, 6));
+                case ComplexType.UpperRight:
+                case ComplexType.BottomRight:
+                case ComplexType.UpperLeft:
+                case ComplexType.BottomLeft:
+                    if (Y > 0) {
+                        return String.Format("{0} + {1}j", Math.Round(X, 6), Math.Round(Y, 6));
+                    } else {
+                        return String.Format("{0} - {1}j", Math.Round(X, 6), Math.Abs(Math.Round(Y, 6)));
+                    }
+                    
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ FFT-DFT/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFT_DFT {
+    class Program {
+        private const char Separator = ' ';
+
+        static void Main(string[] args) {
+            using (StreamReader sr = File.OpenText(@"C:\Users\Adam\OneDrive\UNI\Jelek es rendszerek\feladatok\20\shortSIGN3.txt")) {
+                uint N = 0;
+                double dt = 0.0;
+
+                string szöveg = null;
+                int sor = 0;
+                
+                szöveg = sr.ReadLine();
+                N = uint.Parse(szöveg.Split(separator: Separator)[0]);
+                dt = double.Parse(szöveg.Split(separator: Separator)[1].Replace('.', ','));
+
+                double[] tömb = new double[N];
+                Complex[] dft = new Complex[N];
+                double[] idft = new double[N];
+                uint halfpoint = N / 2 + 1;
+                //if (N % 2 == 0) {
+                //    halfpoint = N / 2 + 1;
+                //} else {
+                //    halfpoint = N / 2;
+                //}
+                double[] samp = new double[halfpoint];
+
+                while (((szöveg = sr.ReadLine()) != null) && (sor < N)) {
+                    tömb[sor] = double.Parse(szöveg.Replace('.', ','));
+                    sor++;
+                }
+
+                for (int k = 0; k < N; k++) {
+                    Complex csum = new Complex(0, 0);
+                    for (int l = 0; l < N; l++) {
+                        Complex complex = new Complex(1, -1 * (2 * Math.PI / N * l * k), false);
+                        csum += new Complex(tömb[l], 0) * complex;
+                    }
+                    dft[k] = csum / new Complex(N, 0);
+                }
+
+                for (int i = 0; i < N; i++) {
+                    Complex csum = new Complex(0, 0);
+                    for (int k = 0; k < N; k++) {
+                        Complex complex = new Complex(1, 2 * Math.PI / N * k * i, false);
+                        Complex szorzás = dft[k] * complex;
+                        csum += szorzás;
+                    }
+                    idft[i] = csum.X;
+                }
+
+                for (int k = 0; k < halfpoint; k++) {
+                    if (k == 0 || (k == N / 2 && N % 2 == 0)) {
+                        samp[k] = dft[k].AbsVal;
+                    } else {
+                        samp[k] = 2 * dft[k].AbsVal;
+                    }
+                }
+
+                Console.WriteLine("DFT:");
+                for (int k = 0; k < N; k++) {
+                    Console.WriteLine("F{0} = {1}", k, dft[k]);
+                }
+
+                Console.WriteLine("\nIDFT");
+                for (int i = 0; i < N; i++) {
+                    Console.WriteLine("f{0} = {1}", i, idft[i]);
+                }
+
+                Console.WriteLine("\nS-AMP");
+                for (int k = 0; k < samp.Length; k++) {
+                    Console.WriteLine("S-AMP: F{0} = {1}", k, samp[k]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
